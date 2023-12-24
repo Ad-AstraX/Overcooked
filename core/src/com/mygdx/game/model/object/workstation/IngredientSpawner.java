d0aab13bbf581f86b3e0eef71fa5f7c271c224da
Ad-AstraX <moushaya@kkg-do.eu>
2023-12-24 00:57:24 -0800

    (fix/IngredientSpawner): optimized IngredientSpawner

--- core/src/com/mygdx/game/model/object/workstation/IngredientSpawner.java
+++ core/src/com/mygdx/game/model/object/workstation/IngredientSpawner.java
@@ -6,29 +6,22 @@ import com.mygdx.game.model.object.holdable.ingredient.*;
 import com.mygdx.game.view.Main;
 
 public class IngredientSpawner<IngredientType> extends KitchenCounter implements IInteractible {
-    private String[] textures = new String[] {"fallbackTexture.png"};
+    private final String[] textures;
     private final Class<IngredientType> ingredientType;
 
     public IngredientSpawner(Class<IngredientType> type, Vector2 position) {
         super("fallbackTexture.png", position, new Vector2(130, 160));
-        if (type.equals(Tomato.class)) {
-            textures = new String[] {"Interactables/Spawner/tomatoSpawner.png", "Interactables/Spawner/tomatoSpawnerSelected.png"};
-        } else if (type.equals(Lettuce.class)) {
-            textures = new String[] {"Interactables/Spawner/lettuceSpawner.png", "Interactables/Spawner/lettuceSpawnerSelected.png"};
-        } else if (type.equals(Patty.class)) {
-            textures = new String[] {"Interactables/Spawner/pattySpawner.png", "Interactables/Spawner/pattySpawnerSelected.png"};
-        } else if (type.equals(Sauce.class)) {
-            textures = new String[] {"Interactables/Spawner/sauceSpawner.png", "Interactables/Spawner/sauceSpawnerSelected.png"};
-        }  else if (type.equals(Bun.class)) {
-            textures = new String[] {"Interactables/Spawner/bunSpawner.png", "Interactables/Spawner/bunSpawnerSelected.png"};
-        }
+        textures = new String[] {
+                "Interactables/Spawner/" + type.getSimpleName().toLowerCase() + "Spawner.png",
+                "Interactables/Spawner/" + type.getSimpleName().toLowerCase() + "SpawnerSelected.png"
+        };
         ingredientType = type;
         this.setTexture(textures[0]);
     }
 
     @Override
     public WorldObject getCopy() {
-        return null;
+        return new IngredientSpawner<>(ingredientType, position);
     }
 
     /**
